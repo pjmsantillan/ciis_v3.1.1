1f7af4f9cc99ebb558597ef6eb2ea662d646584c
jsestoque <jerissa.mae.joelle.estoque@bankofmakati.com.ph>
2025-05-22 15:01:17 +0800

    feat: add DataTableColumnHeader and DataTablePagination components for enhanced data table functionality

--- resources/js/components/data-table.jsx
+++ resources/js/components/data-table.jsx
@@ -5,6 +5,7 @@ import * as React from "react"
 import { Input } from "./ui/input"
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, Search } from "lucide-react"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
+import DataTablePagination from "./data-table-pagination"
 
 export default function DataTable({ columns, data, action }) {
 
@@ -20,17 +21,6 @@ export default function DataTable({ columns, data, action }) {
         getSortedRowModel: getSortedRowModel(),
         onGlobalFilterChange: setGlobalFilter,
         getFilteredRowModel: getFilteredRowModel(),
-        // globalFilterFn: (row, columnId, value) => {
-        //     const searchableFields = ['name', 'email', 'role', 'updated_at', 'created_at']
-        //     return searchableFields.some((field) => {
-        //         const cellValue = row.getValue(field)
-        //         if (typeof cellValue === 'string') {
-        //             return cellValue.toLowerCase().includes(value.toLowerCase())
-        //         }
-        //         return false
-        //     }
-        //     )
-        // },
         state: {
             sorting,
             globalFilter,
@@ -39,7 +29,7 @@ export default function DataTable({ columns, data, action }) {
 
     return (
         <div className="m-4">
-            <div className="flex items-center justify-between p-4">
+            <div className="flex items-center justify-between">
                 <div className="relative flex items-center py-4">
                     <Input type="search"
                         placeholder="Search..."
@@ -53,7 +43,7 @@ export default function DataTable({ columns, data, action }) {
                 </div>
                 {action}
             </div>
-            <div className="border rounded-md p-2">
+            <div className="p-2 border rounded-md">
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
@@ -97,73 +87,7 @@ export default function DataTable({ columns, data, action }) {
                     </TableBody>
                 </Table>
             </div>
-
-            <div className="flex items-center justify-end p-2">
-                <div className="flex items-center space-x-6 lg:space-x-8">
-                    <div className="flex items-center space-x-2">
-                        <p className="text-sm font-medium">Rows per page</p>
-                        <Select
-                            value={`${table.getState().pagination.pageSize}`}
-                            onValueChange={(value) => {
-                                table.setPageSize(Number(value))
-                            }}
-                        >
-                            <SelectTrigger className="h-8 w-[70px]">
-                                <SelectValue placeholder={table.getState().pagination.pageSize} />
-                            </SelectTrigger>
-                            <SelectContent side="top">
-                                {[10, 20, 30, 40, 50].map((pageSize) => (
-                                    <SelectItem key={pageSize} value={`${pageSize}`}>
-                                        {pageSize}
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                    </div>
-                    <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-                        Page {table.getState().pagination.pageIndex + 1} of{" "}
-                        {table.getPageCount()}
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <Button
-                            variant="outline"
-                            className="hidden h-8 w-8 p-0 lg:flex"
-                            onClick={() => table.setPageIndex(0)}
-                            disabled={!table.getCanPreviousPage()}
-                        >
-                            <span className="sr-only">Go to first page</span>
-                            <ChevronsLeft />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            className="h-8 w-8 p-0"
-                            onClick={() => table.previousPage()}
-                            disabled={!table.getCanPreviousPage()}
-                        >
-                            <span className="sr-only">Go to previous page</span>
-                            <ChevronLeft />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            className="h-8 w-8 p-0"
-                            onClick={() => table.nextPage()}
-                            disabled={!table.getCanNextPage()}
-                        >
-                            <span className="sr-only">Go to next page</span>
-                            <ChevronRight />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            className="hidden h-8 w-8 p-0 lg:flex"
-                            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                            disabled={!table.getCanNextPage()}
-                        >
-                            <span className="sr-only">Go to last page</span>
-                            <ChevronsRight />
-                        </Button>
-                    </div>
-                </div>
-            </div>
+            <DataTablePagination table={table} />
         </div >
     )
 }
